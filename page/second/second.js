2f356456ceb601275ba5d7cbd2a256e9115937c7
ILoveHahaha <zhengbanbugu@163.com>
2018-05-17 18:32:04 +0800

    添加功能

--- page/second/second.js
+++ page/second/second.js
@@ -3,16 +3,30 @@ window.onload = function () {
   let selectImg = document.getElementsByClassName('selectImg')[0]
   let state = false
   let clickState = new Set()
+  let paint = document.getElementsByClassName('paint')[0]
+  let currentPaint = {
+    curX1: '',
+    curY1: '',
+    curX2: '',
+    curY2: ''
+  }
+  let movePaint = {
+    curX1: '',
+    curY1: '',
+    curX2: '',
+    curY2: ''
+  }
 
   // 上拉框按钮监听
-  bottom.ontouch = function (ev) {
-    console.log(ev)
+  bottom.onclick = function (ev) {
     let target = ev.target
     let targetName = target.id
+    // console.log(target.style['0'])
+    // console.log(target.name)
+    console.log(target.parentNode.getElementsByTagName('i'))
     if (targetName) {
       document.getElementsByClassName(targetName)[0].style.left = 0
     }
-    console.log(targetName)
     if (target.id === 'pull') {
       if (state) {
         $('#bottom').animate({bottom: '-5.52rem'}, 250)
@@ -22,12 +36,19 @@ window.onload = function () {
         state = true
       }
     } else if (target.nodeName === 'I' && target.id) {
-      for (let a of target.parentNode.getElementsByTagName('i')) {
-        a.classList.remove('active')
+      for (let a = 0; a < target.parentNode.getElementsByTagName('i').length; a++) {
+        target.parentNode.getElementsByTagName('i')[a].classList.remove('active')
         selectImg.removeEventListener('touchstart', function () {})
         selectImg.removeEventListener('touchmove', function () {})
         selectImg.removeEventListener('touchend', function () {})
       }
+      // for (let a of target.parentNode.getElementsByTagName('i')) {
+      //   console.log(a)
+        // a.classList.remove('active')
+        // selectImg.removeEventListener('touchstart', function () {})
+        // selectImg.removeEventListener('touchmove', function () {})
+        // selectImg.removeEventListener('touchend', function () {})
+      // }
       target.classList.add('active')
       if (!clickState.has(targetName)) {
         switch (target.id) {
@@ -62,10 +83,16 @@ window.onload = function () {
         }
       }
       clickState.add(targetName)
-      for (let a of document.getElementsByClassName('icon')) {
-        a.style.display = 'none'
+      for (let a = 0; a < document.getElementsByClassName('icon').length; a++) {
+        document.getElementsByClassName('icon')[a].style.display = 'none'
       }
+      // for (let a of document.getElementsByClassName('icon')) {
+      //   a.style.display = 'none'
+      // }
       document.getElementsByClassName(target.id)[0].style.display = 'flex'
+    } else if (target.nodeName === 'I' && target.style['0']) {
+      let url = target.style.backgroundImage.split('second/')[1].split('")')[0].split('/')
+      drawImage(url)
     } else if (target.nodeName === 'I' && target.name) {
       switch (target.name) {
         case 'tile':
@@ -82,6 +109,38 @@ window.onload = function () {
     }
   }
 
+  // 画图去监听
+  // paint.onclick = function (ev) {
+  //   let target = ev.target
+  // }
+  paint.addEventListener('touchstart', function (ev) {
+    let touches = ev.targetTouches
+    let left = document.getElementsByClassName('left')[0].style.width
+    let top = document.getElementsByClassName('title')[0].style.height * 0.9
+    currentPaint.curX1 = touches[0].clientX - left
+    currentPaint.curY1 = touches[0].clientY - top
+    paint.addEventListener('touchmove', function (ev) {
+      // console.log(ev.target.tagName)
+      if (touches.length === 1) {
+        move(ev, left, top)
+      } else if (touches.length === 2) {
+        // currentPaint.curX2 = touches[1].clientX - document.getElementsByClassName('left')[0].style.width
+        // currentPaint.curY2 = touches[1].clientY - document.getElementsByClassName('title')[0].style.height * 0.9
+        if (touches[0].target.tagName === 'I' && touches[1].target.tagName === 'I') {
+
+        } else if (touches[0].target.tagName === 'P' && touches[1].target.tagName === 'P') {
+
+        }
+      }
+    })
+    paint.addEventListener('touchend', function (ev) {
+      
+    })
+    // console.log(touches[0].target.tagName)
+
+    // console.log(ev.targetTouches)
+  })
+
   // 动态创建元素
   function createIcon(targetName, num) {
     for (let a = 1; a <= num; a++) {
@@ -97,7 +156,7 @@ window.onload = function () {
   function scrollIcon(targetName, endWidth) {
     let selectLeft = 0
     selectImg.addEventListener('touchstart', function (ev) {
-      selectLeft = document.getElementsByClassName(targetName)[0].style.left.split('rem')[0]
+      selectLeft = document.querySelectorAll(`div.${targetName}`)[0].style.left.split('rem')[0]
       selectLeft = selectLeft ? parseFloat(selectLeft) : 0
       let primary = ev.touches[0].clientX
       let drap = 0
@@ -108,26 +167,62 @@ window.onload = function () {
         } else if (drap < endWidth - 3) {
           drap = endWidth - 3
         }
-        document.getElementsByClassName(targetName)[0].style.left = drap + 'rem'
+        document.querySelectorAll(`div.${targetName}`)[0].style.left = drap + 'rem'
       })
       selectImg.addEventListener('touchend', function () {
         if (drap > 0) {
-          document.getElementsByClassName(targetName)[0].style.left = 0
+          document.querySelectorAll(`div.${targetName}`)[0].style.left = 0
         } else if (drap < endWidth) {
-          document.getElementsByClassName(targetName)[0].style.left = endWidth + 'rem'
+          document.querySelectorAll(`div.${targetName}`)[0].style.left = endWidth + 'rem'
         }
       })
     })
   }
   
   // 画图
-  function drawImage() {
-    
+  function drawImage({0: path, 1: name}) {
+    let selectIcon = document.querySelectorAll(`p.${path}`)
+    if (path === 'sky' || path === 'farm') {
+      if (selectIcon.length > 0) {
+        selectIcon[0].childNodes[0].style.background = `url(../../img/second/${path}/${name}`
+        selectIcon[0].childNodes[0].style.backgroundSize = '100% 100%'
+      } else {
+        let p = document.createElement('p')
+        let i = document.createElement('i')
+        p.className = path
+        i.style.height = '3.5rem'
+        i.style.width = '5rem'
+        p.style.marginTop = '-2.25rem'
+        p.style.marginLeft = '-3rem'
+        p.style.border = '1px dashed #CAFF70'
+        i.style.background = `url(../../img/second/${path}/${name}`
+        i.style.backgroundSize = '100% 100%'
+        p.appendChild(i)
+        paint.appendChild(p)
+      }
+    } else {
+
+    }
   }
   
   // 放大缩小
   function drop() {
-    
+
+  }
+  
+  // 移动
+  function move(ev, left, top) {
+    console.log(ev.targetTouches[0])
+    movePaint.curX1 = ev.targetTouches[0].clientX - left
+    movePaint.curY1 = ev.targetTouches[0].clientY - top
+    // if (touches[0].target.tagName === 'I') {
+    if (ev.target.nodeName === 'I') {
+      ev.target.parentNode.style.left = movePaint.curX1 + 'px'
+      ev.target.parentNode.style.top = movePaint.curY1 + 'px'
+    } else if (ev.target.nodeName === 'P') {
+      ev.target.style.left = movePaint.curX1 + 'px'
+      ev.target.style.top = movePaint.curY1 + 'px'
+    }
   }
   
   // 旋转
